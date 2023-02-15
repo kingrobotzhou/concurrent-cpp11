fc56f8ba2a92fce7325abaf5a35f3781f2d966de
kingrobotzhou <kingrobot@protonmail.com>
2023-02-15 09:22:19 +0800

    添加wait函数家族的备注。

--- demo15/main.cpp
+++ demo15/main.cpp
@@ -18,9 +18,16 @@ static int block_time = 20;
 
 （2） wait(lock, conditions): 调用时检查conditions，如果为false,则阻塞线程，并且调用lock.unlock(), 否则，继续执行、
 
-（3） wait_for(lock, time_duration, conditions): 调用时，检查条件是否满足:(1) conditions返回true; (2) 时间超时，如果不满足(1)(2)中的一个条件，则阻塞线程，并调用lock.unlock(), 否则，到达一定等待时间或满足条件被唤醒 ，注意，等待超过时间段后自动唤醒，判断条件一般需要使用者自己在合适的时候判断，并通过notify_one()或notify_all()唤醒，所以，使用的时候注意判断返回值，即状态是否为std::cv_status::timeout
+（3） wait_for(lock, time_duration, conditions): 调用时，
+     检查条件是否满足:(1) conditions返回true; (2) 时间超时，如果不满足(1)(2)中的一个条件，则阻塞线程，
+     并调用lock.unlock(), 否则，到达一定等待时间或满足条件被唤醒 ，注意，等待超过时间段后自动唤醒，
+     判断条件一般需要使用者自己在合适的时候判断，并通过notify_one()或notify_all()唤醒，
+     所以，使用的时候注意判断返回值，即状态是否为std::cv_status::timeout
 
-（4） wait_until(lock, time_point, conditions): 实际wait_for是通过wait_until实现，实际上也是一样的，到达指定时间点或满足条件conditions时被唤醒，注意，到达时间点是自动唤醒，判断条件一般需要使用者自己在合适的时候判断，并通过notify_one()或notify_all()唤醒，所以，使用的时候注意判断返回值，即状态是否为std::cv_status::timeout
+（4） wait_until(lock, time_point, conditions): 实际wait_for是通过wait_until实现，
+     实际上也是一样的，到达指定时间点或满足条件conditions时被唤醒，注意，到达时间点是自动唤醒，
+     判断条件一般需要使用者自己在合适的时候判断，并通过notify_one()或notify_all()唤醒，所以，
+     使用的时候注意判断返回值，即状态是否为std::cv_status::timeout
 
 */
  
